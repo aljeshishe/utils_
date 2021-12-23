ba7ed331899522ed23b5dd56007cb85684e85d90
Alexey Grachev <ax66@bk.ru>
2021-12-23 19:00:40 +0300

    added pandas.i pandas.d pandas.drop_from

--- utils/pandas.py
+++ utils/pandas.py
@@ -9,19 +9,33 @@ def reorder_columns(self, *columns):
     new_columns = list(columns) + [item for item in self.columns if item not in columns]
     return self[new_columns]
 
-def show(df, count=5, dtale=False):
+
+def d(df):
     dtale_ = utils.imp.install_import('dtale', VERSIONS['dtale'])
-    if dtale:
-        dtale_.show(df).open_browser()
-    else:
-        print(f'{len(df)} rows')
-        display(df.head(count))
+    dtale_.show(df).open_browser()
+
+
+def i(df, count=5):
+    print(f'{len(df)} rows')
+    display(df.head(count))
+
+
+def drop_from(query_df, df):
+    before = len(df)
+    print('Going to drop')
+    query_df.i()
+    df = df.drop(query_df.index)
+    after = len(df)
+    print(f'Before:{before} after:{after}')
+    return df
 
 
 def patch():
     print('patching pandas')
     pd.core.frame.DataFrame.reorder_columns = reorder_columns
-    pd.core.frame.DataFrame.show = show
+    pd.core.frame.DataFrame.d = d
+    pd.core.frame.DataFrame.i = i
+    pd.core.frame.DataFrame.drop_from = drop_from
 
 
 def download(url, headers={}):
