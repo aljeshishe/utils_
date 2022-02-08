059f60e6b14bcab63f77b1be9f49a13d526cd8e1
Alexey Grachev <ax66@bk.ru>
2022-02-08 18:53:20 +0300

    added log configure

--- utils/log.py
+++ utils/log.py
@@ -1,7 +1,11 @@
+import logging.config
+import pathlib
+from datetime import datetime
 import functools
 import logging
 
 
+print('log')
 log = logging.getLogger(__name__)
 
 
@@ -12,3 +16,54 @@ def trace(f):
         return f(*args, **kwargs)
     return wrapper
 
+
+def default_config(filename='logs/log', console_level='INFO'):
+    log_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s.%(msecs)03d|%(levelname)-4.4s|%(thread)-6.6s|%(module)-6.6s|%(funcName)-10.10s|%(message)s',
+                'datefmt': '%Y/%m/%d %H:%M:%S',
+            },
+        },
+        'handlers': {
+            'file_handler': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': f'{filename}_%s.log' % datetime.now().strftime("%d%m%y_%H%M%S"),
+                'formatter': 'verbose',
+                'mode': 'w',
+                'encoding': 'utf8',
+            },
+            'console_handler': {
+                'level': console_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose',
+            },
+        },
+        'loggers': {
+            'requests': {
+                'handlers': ['file_handler', 'console_handler'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'urllib3': {
+                'handlers': ['file_handler', 'console_handler'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        },
+        'root': {
+            'handlers': ['file_handler', 'console_handler'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    }
+    return log_config
+
+
+def configure(config):
+    file_name = config['handlers']['file_handler']['filename']
+    pathlib.Path(file_name).parent.mkdir(parents=True, exist_ok=True)
+    logging.config.dictConfig(config)
